72be5c8d8f8ace68cf574e71f6f8beafd6eec53b
Madhukar-VS <madhukar.k22@gmail.com>
2020-06-09 11:26:56 +0530

    test34

--- nodeSample.js
+++ nodeSample.js
@@ -6,5 +6,5 @@ app.get('/', function(req,res) {
     res.send('hello welcome to my new world');    
 })
 app.listen(3002, function(req,res) {
-    console.log("Hello I am on running on port 3002");
+    console.log("Hillo I am on running on port 3002");
 });
