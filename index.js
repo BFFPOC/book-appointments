8ecdf6690d009c65e5b9779e3193ccb238949e76
samplezaurenode <duggi.saiteja@gmail.com>
2020-06-24 21:43:47 +0530

    added JWT token

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const http = require('./httpserver');
-const Port = process.env.PORT || 1311
+const Port = process.env.PORT || 3000
 
 http.listen(Port, function (req, res) {
     console.log('helo I am new server');
